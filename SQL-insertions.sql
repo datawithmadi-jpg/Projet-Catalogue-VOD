bc1ce370a811627fa0d453309e2999c8e3c6757a
datawithmadi-jpg <datawithmadi@gmail.com>
2026-03-25 00:47:27 +0100

    Create SQL-insertions.sql

---
+++ SQL-insertions.sql
@@ -0,0 +1,24 @@
+INSERT INTO clients (
+    code_client,
+    civilite,
+    prenom,
+    nom,
+    adresse_rue,
+    code_postal,
+    ville,
+    num_telephone,
+    date_naissance,
+    enfants
+)
+VALUES (
+    'ABO001',
+    'Mme',
+    'Elisabeth',
+    'ABONDIEU',
+    '5, place de la Cathédrale',
+    '44000',
+    'NANTES',
+    '0240223311',
+    'janv  1 1956 12:00AM',
+    0
+);
